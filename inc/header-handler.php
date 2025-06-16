9db3475fba1fc8adf39adab0d0515bfecfbb6c3e
Robert Allen Baker <robertallenbaker@me.com>
2025-06-16 09:41:59 -0400

    Fixed PHP Warning on header handler

--- inc/header-handler.php
+++ inc/header-handler.php
@@ -2,8 +2,8 @@
 function header_hub_get_custom_header($default_header = '')
 {
     global $post;
-
-    $custom_header = get_post_meta($post->ID, '_custom_header', true);
+    $post_id = (isset($post) && is_object($post) && isset($post->ID)) ? $post->ID : null;
+    $custom_header = $post_id ? get_post_meta($post_id, '_custom_header', true) : '';
     if ($custom_header && $custom_header !== 'default') {
         if ($custom_header === 'header') {
             get_header();
@@ -19,8 +19,8 @@ function header_hub_get_custom_header($default_header = '')
 function footer_hub_get_custom_footer($default_footer = '')
 {
     global $post;
-
-    $custom_footer = get_post_meta($post->ID, '_custom_footer', true);
+    $post_id = (isset($post) && is_object($post) && isset($post->ID)) ? $post->ID : null;
+    $custom_footer = $post_id ? get_post_meta($post_id, '_custom_footer', true) : '';
     if ($custom_footer && $custom_footer !== 'default') {
         if ($custom_footer === 'footer') {
             get_footer();
